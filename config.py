6851478c2a4203140c0adddd2222f4b8d5cd3244
MrRedPandabaer <42328017+MrRedPandabaer@users.noreply.github.com>
2019-01-27 13:46:06 +0100

    Updated config.py for OPQ

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 # fill in these variables
-limit = 10000
+limit = 100000
 transactions = 500
-contract = ''
+contract = '0x77599d2c6db170224243e255e6669280f11f1473'
 looptime = 30
 my_token = ''
-chat_id = ''
-filename = ''
+filename = 'transactionHistory'
 api_key = ''
